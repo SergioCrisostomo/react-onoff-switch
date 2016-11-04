66121dfed91d610bc6a74fef8662daf6caab7005
Sergio Crisostomo <sergiosbox@gmail.com>
2016-11-04 17:41:20 +0100

    add demo

---
+++ demo.js
@@ -0,0 +1,17 @@
+
+import ReactDOM from 'react-dom';
+import OnOff from './index.js'
+
+const stateDiv = document.getElementById('state');
+const reporter = who => val => stateDiv.innerHTML = `${who} changed value to ${val}!`;
+
+ReactDOM.render(
+	<div>
+		<OnOff onChange={reporter('Blue button')} activeColor={'#CCE'} on={true}/>
+		<p/>
+		<OnOff onChange={reporter('Green button')}/>
+	</div>,
+	document.getElementById('app')
+);
+
+export default {};
